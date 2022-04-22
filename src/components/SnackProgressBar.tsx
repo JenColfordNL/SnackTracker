bec91330fc01121923b26d402451cd87fc6ead3c
jennifer colford <jennifer.colford@keyin.com>
2022-04-22 18:32:29 -0230

    Change default snacks to 5

--- src/components/SnackProgressBar.tsx
+++ src/components/SnackProgressBar.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { GiStarFormation } from 'react-icons/gi';
+import { useState, useEffect } from "react";
+import { GiStarFormation } from "react-icons/gi";
 
 export default function SnackProgressBar({ count, targetSnacks }) {
-  const targetSnacksDefault = 4;
+  const targetSnacksDefault = 5;
 
   const [percentProgress, setPercentProgress] = useState(0);
 
@@ -10,7 +10,8 @@ export default function SnackProgressBar({ count, targetSnacks }) {
     if (count < 0) count = 0;
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    if (targetSnacks < 1 || targetSnacks > targetSnacksDefault) targetSnacks = targetSnacksDefault;
+    if (targetSnacks < 1 || targetSnacks > targetSnacksDefault)
+      targetSnacks = targetSnacksDefault;
 
     if (count > targetSnacks) count = targetSnacks;
 
@@ -31,13 +32,19 @@ export default function SnackProgressBar({ count, targetSnacks }) {
           </div>
         </div>
         <div>
-          {percentProgress !== 100 && count === 0 && <p>What will be your first snack today?</p>}
+          {percentProgress !== 100 && count === 0 && (
+            <p>What will be your first snack today?</p>
+          )}
 
           {percentProgress !== 100 && count === 1 && <p>Great Start!!!</p>}
 
-          {percentProgress !== 100 && count > 1 && <p>Excellent, {count} snacks gone!</p>}
+          {percentProgress !== 100 && count > 1 && (
+            <p>Excellent, {count} snacks gone!</p>
+          )}
 
-          {percentProgress === 100 && <p>Congratulations Jack, all snacks eaten!!!</p>}
+          {percentProgress === 100 && (
+            <p>Congratulations Jack, all snacks eaten!!!</p>
+          )}
         </div>
       </div>
     </div>
@@ -47,7 +54,10 @@ export default function SnackProgressBar({ count, targetSnacks }) {
 const ProgressBar = ({ progressPercentage }) => {
   return (
     <div className="progressbar-outer">
-      <div style={{ width: `${progressPercentage}%` }} className="progressbar-inner"></div>
+      <div
+        style={{ width: `${progressPercentage}%` }}
+        className="progressbar-inner"
+      ></div>
     </div>
   );
 };
